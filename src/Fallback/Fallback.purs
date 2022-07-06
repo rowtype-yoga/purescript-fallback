2a0f0389df612b5c690af1a79bc768b5cfe45337
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-07-06 10:05:50 +0100

    Add function comments

--- src/Fallback/Fallback.purs
+++ src/Fallback/Fallback.purs
@@ -7,13 +7,37 @@ import Control.Monad.Error.Class (class MonadError, try)
 import Data.Either (either)
 import Data.Traversable (class Traversable, sequence)
 
-fallback :: forall m e input output. MonadError e m => Traversable m => m input -> (e -> m output) -> ContT output m input
+-- | Enable `fallback`s for a do-comprehension
+-- |
+-- | Usage: 
+-- | ```
+-- | withFallback do
+-- |   result <- someM |> someErrorHandler 
+-- | ```
+withFallback
+  :: forall output m
+   . Applicative m
+  => ContT output m output
+  -> m output
+withFallback = flip runContT (identity >>> pure)
+
+-- | Create a fallback for an `m a` using the given error handler.
+-- |
+-- | Usage: 
+-- | ```
+-- | withFallback do
+-- |   result <- someM `fallback` someErrorHandler
+-- | ```
+fallback
+  :: forall m e input output
+   . MonadError e m
+  => Traversable m
+  => m input
+  -> (e -> m output)
+  -> ContT output m input
 fallback ma errorHandler = try ma # sequence # either left right
   where
   left = \e -> ContT $ const $ errorHandler e
   right = lift
-
-withFallback :: forall output m. Applicative m => ContT output m output -> m output
-withFallback = flip runContT (identity >>> pure)
 
 infixl 1 fallback as |>
